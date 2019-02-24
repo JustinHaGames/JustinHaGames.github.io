20061d7e3e376e00e3e42ced1fd96b01d6823d01
Justin Ha <justinha22@gmail.com>
2019-02-24 13:17:24 -0500

    Fixed up homepage text

--- Javascript/Homepage.js
+++ Javascript/Homepage.js
@@ -1,6 +1,6 @@
 const text = 'Hello my name is Justin and I';
 
-const varyingText = ['am a Game Designer.', 'like creating games about complex emotions.', 'am getting better at Pixel Art.', 'love Anthropology.', 'am constantly learning new things.', 'am getting the hang of Web Design', 'have two cats I am allergic to. <3'];
+const varyingText = ['am a Game Designer.', 'like creating games about complex emotions.', 'am getting better at Pixel Art.', 'love Anthropology.', 'am constantly learning new things.', 'am getting the hang of Web Design.', 'play a lot of competitive games.'];
 
 const totalText = text + ' ' + varyingText[0];
 
